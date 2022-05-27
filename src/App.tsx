3b89a2ca762c1f79d22987e650c4a60ae4e999ab
Daniil <dfefelov@bk.ru>
2022-05-27 11:34:33 +0700

    Lesson 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {OnOff} from "./components/OnOff/OnOff";
+import {UnControlledAccordion} from "./components/UnControlledAccordion/UnControlledAccordion";
+import {UnControlledRating} from "./components/UnControlledRating/UnControlledRating";
+import {Accordion} from "./components/Accordion/Accordion";
+import {UnControlledOnOff} from "./components/UnControlledOnOff/UnControlledOnOff";
+
+export type RatingValueType = 0 | 1 | 2 | 3 | 4 | 5
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [ratingControlled, setRatingControlled] = useState<RatingValueType>(0)
+    const [accordionControlled, setAccordionControlled] = useState<boolean>(false)
+    const [accordionUnControlled, setAccordionUnControlled] = useState<boolean>(false)
+    let [on, setOn] = useState<boolean>(false)
+    let [switchOn, setSwitchOn] = useState<boolean>(false)
+
+    return (
+        <div className={"App"}>
+            <Accordion
+                titleValue={"Menu1"}
+                accordionControlled={accordionControlled}
+                changeCollapsed={() => setAccordionControlled(!accordionControlled)}/>
+            <UnControlledAccordion
+                titleValue={"Menu2"}
+                accordionUnControlled={accordionUnControlled}
+                setAccordionUnControlled={setAccordionUnControlled}/>
+            <UnControlledRating
+                ratingControlled={ratingControlled}
+                setRatingControlled={setRatingControlled}/>
+            <OnOff
+                switchOn={switchOn}
+                setSwitchOn={() => setSwitchOn(!switchOn)}/>
+            <UnControlledOnOff
+                on={on}
+                setOn={setOn}/>
+            <p>{on ? "true" : "false"}</p>
+
+            {/*<PageTitle title={"This is APP component"}/>*/}
+            {/*<Rating value={0}/>*/}
+        </div>
+    );
+}
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    return <h1>{props.title}</h1>
 }
 
 export default App;
